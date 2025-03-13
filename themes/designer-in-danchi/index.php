3398ea3d931936337070979b15f0cd3f11ce32af
mazaka <mazaka@mazakanoMacBook-Pro.local>
2025-03-13 21:01:09 +0900

    最初のプッシュですよ

--- themes/designer-in-danchi/index.php
+++ themes/designer-in-danchi/index.php
@@ -1,10 +1,7 @@
 <div id="top" class="cover-container d-flex w-100 h-100 mx-auto flex-column">
-
     <?php get_header(); ?>
     <main class="h-100 d-flex justify-content-center align-items-center">
-         <h1 class="fs-3 fw-light pb-5"><a href="/about/">Simple but creative living.</a></h1>
+        <h1 class="fs-3 fw-light pb-5"><a href="/about/">Simple but creative living.</a></h1>
     </main>
-
-
     <?php get_footer(); ?>
 </div>
